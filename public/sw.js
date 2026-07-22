1d45520f82f851ac1916e401bd426c12f8c4f625
q919237359-ops <290984868+q919237359-ops@users.noreply.github.com>
2026-07-22 11:54:24 +0800

    fix: clear legacy service worker cache

---
+++ public/sw.js
@@ -0,0 +1,14 @@
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil((async () => {
+    const cacheNames = await caches.keys();
+    await Promise.all(cacheNames.map((name) => caches.delete(name)));
+    await self.registration.unregister();
+
+    const windows = await self.clients.matchAll({ type: "window" });
+    await Promise.all(windows.map((client) => client.navigate(client.url)));
+  })());
+});
